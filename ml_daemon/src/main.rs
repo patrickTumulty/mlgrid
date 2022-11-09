ed3d6fd47f3220c6a46efd27c5f288b91db7236e
ptumulty <ptumulty1@gmail.com>
2022-11-08 23:26:13 -0800

    Added randomize array

--- ml_daemon/src/main.rs
+++ ml_daemon/src/main.rs
@@ -1,4 +1,17 @@
+use mlrust::NeuralNetwork;
+
 fn main() {
     let a = mlrust::math_utils::sig(5.43);
     println!("{}", a);
+
+
+    let mut nn = NeuralNetwork::new(3, 2, vec![4]);
+
+    nn.init_network();
+
+    let len = (&nn.layers()).len().to_owned();
+    for i in 0..len {
+        println!("{:4.4}\n", nn.layers()[i].connection_weights());
+    }
+
 }
