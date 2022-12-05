c2b06d6a50b01f3d15532ab3eea4b0c1a526acc2
ptumulty <ptumulty1@gmail.com>
2022-12-04 20:46:50 -0800

    Writing back prop as recursive algorithm

--- mldriver/src/main.rs
+++ mldriver/src/main.rs
@@ -5,52 +5,75 @@ use mlrust::{array_utils, ColumnVector, NeuralNetwork};
 
 fn main() {
 
-    test_backprop();
+    // back_prop_test();
+    //
+    train_binary_function();
 
-
-    run_procedural_math();
+    // test_backprop();
+    //
+    // run_procedural_math();
 }
 
-fn test_backprop() {
+fn back_prop_test() {
     let mut input: ColumnVector = ColumnVector::zeros(2);
-    input[0] = 0.05;
-    input[1] = 0.10;
-    let mut weights_l1: Array2<f32> = Array2::zeros((2, 2));
-    weights_l1[[0, 0]] = 0.15;
-    weights_l1[[0, 1]] = 0.20;
-    weights_l1[[1, 0]] = 0.25;
-    weights_l1[[1, 1]] = 0.30;
-    let mut biases_l1: ColumnVector = ColumnVector::zeros(2);
-    biases_l1[0] = 0.35;
-    biases_l1[1] = 0.46;
-
-
+    input[0] = 0.1;
+    input[1] = 0.5;
     let mut expected: ColumnVector = ColumnVector::zeros(2);
-    expected[0] = 0.01;
-    expected[1] = 0.99;
+    expected[0] = 0.05;
+    expected[1] = 0.95;
 
+    // Layer One
+    let mut weights_l1: Array2<f32> = Array2::zeros((2, 2));
+    weights_l1[[0, 0]] = 0.1;
+    weights_l1[[0, 1]] = 0.3;
+    weights_l1[[1, 0]] = 0.2;
+    weights_l1[[1, 1]] = 0.4;
+    let mut biases_l1: ColumnVector = ColumnVector::zeros(2);
+    biases_l1[0] = 0.25;
+    biases_l1[1] = 0.25;
+
+    // Layer Two
     let mut weights_l2: Array2<f32> = Array2::zeros((2, 2));
-    weights_l2[[0, 0]] = 0.40;
-    weights_l2[[0, 1]] = 0.45;
-    weights_l2[[1, 0]] = 0.50;
-    weights_l2[[1, 1]] = 0.55;
+    weights_l2[[0, 0]] = 0.5;
+    weights_l2[[0, 1]] = 0.6;
+    weights_l2[[1, 0]] = 0.7;
+    weights_l2[[1, 1]] = 0.8;
     let mut biases_l2: ColumnVector = ColumnVector::zeros(2);
-    biases_l2[0] = 0.60;
-    biases_l2[1] = 0.70;
+    biases_l2[0] = 0.35;
+    biases_l2[1] = 0.35;
 
-    let weights = vec![weights_l1, weights_l2];
-    let biases = vec![biases_l1.get_data().to_owned(), biases_l2.get_data().to_owned()];
+    let mut nn = NeuralNetwork::from(vec![weights_l1, weights_l2], vec![biases_l1.get_data().clone(), biases_l2.get_data().clone()]);
 
-    let mut nn = NeuralNetwork::from(weights, biases);
+    nn.back_propagate(input.get_data(), expected.get_data());
 
-    // for layer in nn.layers() {
-    //     println!("{}", *layer);
-    // }
+}
 
-    let result: (Vec<Array2<f32>>, Vec<Array2<f32>>) = nn.back_propagate(&input, &expected);
-    for i in (0..nn.layers().len()).rev() {
-        println!("{}\n{}\n{}", i + 1, result.0[i], result.1[i]);
+fn train_binary_function() {
+    let mut nn = NeuralNetwork::new(4, 16, vec![10, 10]);
+
+    let mut inputs: Vec<ColumnVector> = Vec::with_capacity(16);
+    let mut outputs: Vec<ColumnVector> = Vec::with_capacity(16);
+    for i in 0..16 {
+        let mut col_in = ColumnVector::zeros(4);
+        col_in[0] = (i & 0b1) as f32;
+        col_in[1] = (i >> 1 & 0b1) as f32;
+        col_in[2] = (i >> 2 & 0b1) as f32;
+        col_in[3] = (i >> 3& 0b1) as f32;
+        inputs.push(col_in);
+        let mut col_out = ColumnVector::zeros(16);
+        col_out[i] = 1.0;
+        outputs.push(col_out);
     }
+
+    for i in 0..1000 {
+        nn.train(&inputs, &outputs);
+    }
+
+    let mut test1 = ColumnVector::zeros(4);
+    test1[1] = 1.0;
+    println!("2\n{}", nn.feed_forward(test1.clone()));
+    test1[0] = 1.0;
+    println!("3\n{}", nn.feed_forward(test1.clone()));
 }
 
 
