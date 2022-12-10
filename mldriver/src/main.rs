344b034641e06c8a6466de4057e6f553b036def9
ptumulty <ptumulty1@gmail.com>
2022-12-10 15:16:06 -0800

    [GM-2] Initial implementation of network serialization

--- mldriver/src/main.rs
+++ mldriver/src/main.rs
@@ -1,9 +1,21 @@
-use graymat::examples;
+
+use graymat::neural_network::NeuralNetwork;
+use graymat::neural_network_io::{from_file, to_file};
+
 
 fn main() {
-    examples::binary_to_int();
+    // examples::binary_to_int();
+    //
+    // examples::xor();
 
-    examples::xor();
+
+    let nn1 = NeuralNetwork::new(3, 5, vec![3, 4]);
+    println!("{}", nn1);
+    to_file("./test2.nn".to_owned(), nn1);
+
+    let nn2 = from_file("./test2.nn".to_owned());
+    println!("{}", nn2);
+
 }
 
 
