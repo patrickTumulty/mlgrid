d91436b24025110e606d5f3c8c7fde76804ad4b9
ptumulty <ptumulty1@gmail.com>
2023-01-16 22:44:47 -0800

    Add routes to mlapp

--- mlapp/src/App.js
+++ mlapp/src/App.js
@@ -1,10 +1,16 @@
 import './css/App.css';
 import GridCanvas from "./GridCanvas";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <GridCanvas/>
+            <Router>
+                <Routes>
+                    <Route exact path='/' element={<GridCanvas/>}/>
+                </Routes>
+            </Router>
     </div>
   );
 }
